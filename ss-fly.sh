a298d635f6f12aadf23919caf5bab10f379ba956
konggefd <37166538+konggefd@users.noreply.github.com>
2018-04-21 07:49:07 +0800

    Update ss-fly.sh

--- ss-fly.sh
+++ ss-fly.sh
@@ -42,46 +42,72 @@ install() {
 	pip install shadowsocks
 	chmod 755 /etc/shadowsocks.json
 	apt-get install python-m2crypto
-	ssserver -c /etc/shadowsocks.json -d start
+        command -v ssserver >/dev/null 2>&1 || { echo >&2 "请确保你服务器（服务器，不是你自己的电脑）的系统是Ubuntu。如果系统是Ubuntu，似乎因为网络原因ss没有安装成功，请再执行一次搭建ss脚本代码。如果试了几次还是不行，执行reboot命令重启下服务器之后再试下，如果还是不行，请联系flyzy小站。"; exit 1; }
+	ps -fe|grep ssserver |grep -v grep > /dev/null 2>&1
+        if [ $? -ne 0 ]
+        then
+          ssserver -c /etc/shadowsocks.json -d start
+        else
+          ssserver -c /etc/shadowsocks.json -d restart
+        fi
 	rclocal=`cat /etc/rc.local`
         if [[ $rclocal != *'ssserver -c /etc/shadowsocks.json -d start'* ]]
         then
           sed -i '$i\ssserver -c /etc/shadowsocks.json -d start'  /etc/rc.local
         fi
-	echo '安装成功~尽情冲浪吧'
+	echo "安装成功~尽情冲浪吧
+您的配置文件内容如下（server在客户端中需要配置成你VPS的IP）："
+	cat /etc/shadowsocks.json
 }
 
 install_bbr() {
-i=`uname -r | cut -f 2 -d .`
-if [ $i -le 9 ]
-then
-    if
-        echo '准备下载镜像文件...' && wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.2/linux-image-4.10.2-041002-generic_4.10.2-041002.201703120131_amd64.deb
+	sysfile=`cat /etc/sysctl.conf`
+	if [[ $sysfile != *'net.core.default_qdisc=fq'* ]]
+	then
+    		echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+	fi
+	if [[ $sysfile != *'net.ipv4.tcp_congestion_control=bbr'* ]]
+	then
+    		echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+	fi
+	sysctl -p > /dev/null
+	i=`uname -r | cut -f 2 -d .`
+	if [ $i -le 9 ]
+	then
+    		if
+        	echo '准备下载镜像文件...' && wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.10.2/linux-image-4.10.2-041002-generic_4.10.2-041002.201703120131_amd64.deb
+    		then
+        		echo '镜像文件下载成功，开始安装...' && dpkg -i linux-image-4.10.2-041002-generic_4.10.2-041002.201703120131_amd64.deb && update-grub && echo '镜像安装成功，系统即将重启，重启后bbr将成功开启...' && reboot
+    		else
+        		echo '下载内核文件失败，请重新执行安装BBR命令'
+        		exit 1
+    		fi
+	fi
+	result=`sysctl net.ipv4.tcp_available_congestion_control`
+	if [[ $result == *'bbr'* ]]
+	then
+    		echo 'BBR已开启成功'
+	else 
+    		echo 'BBR开启失败，请重试'
+	fi
+}
 
-    then
-        echo '镜像文件下载成功，开始安装...' && dpkg -i linux-image-4.10.2-041002-generic_4.10.2-041002.201703120131_amd64.deb && update-grub && echo '镜像安装成功，准备重启...' && reboot
-    else
-        echo '下载内核文件失败，请重新执行安装BBR命令'
-        exit 1
-    fi
-fi
-sysfile=`cat /etc/sysctl.conf`
-if [[ $sysfile != *'net.core.default_qdisc=fq'* ]]
-then
-    echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
-fi
-if [[ $sysfile != *'net.ipv4.tcp_congestion_control=bbr'* ]]
-then
-    echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
-fi
-sysctl -p > /dev/null
-result=`sysctl net.ipv4.tcp_available_congestion_control`
-if [[ $result == *'bbr'* ]]
-then
-    echo 'BBR开启成功'
-else 
-    echo 'BBR开启失败，请重试'
-fi
+install_ssr() {
+	wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocksR.sh
+	chmod +x shadowsocksR.sh
+	./shadowsocksR.sh 2>&1 | tee shadowsocksR.log
+}
+
+uninstall_ss() {
+	ps -fe|grep ssserver |grep -v grep > /dev/null 2>&1
+        if [ $? -eq 0 ]
+        then
+          ssserver -c /etc/shadowsocks.json -d stop
+        fi
+	pip uninstall -y shadowsocks
+	rm /etc/shadowsocks.json
+	rm /var/log/shadowsocks.log
+	echo 'shadowsocks卸载成功'
 }
 
 if [ "$#" -eq 0 ]; then
@@ -112,6 +138,12 @@ case $1 in
         -bbr )
         install_bbr
                 ;;
+        -ssr )
+        install_ssr
+                ;;
+	-uninstall )
+	uninstall_ss
+		;;
 	* )
 		usage
 		;;
